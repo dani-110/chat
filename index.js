e78d7189767039f1608bc46d05f775674eabfbfd
java@012 <daniyal.digitalwaze@fmail.com>
2021-06-01 14:28:15 +0500

    add chat app

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const socket = require('socket.io')
+const express = require('express')
+const path =require('path')
+
+const publicPath = path.join(__dirname, '/public')
+const app = express()
+let server = http.createServer(app);
+let io = socket(server);
+
+app.use(express.static(publicPath))
+
+io.on('connection', (socket) => {
+    console.log('Connected.....')
+//   socket.on('event', data => { console.log(data) });
+  socket.on('disconnect', () => { 
+      console.log("disconnected")
+   });
+});
+
+server.listen(3000,()=>{
+    console.log("hello activated", publicPath)
+});
+
