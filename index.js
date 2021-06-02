8dc273b86a28ce46594d0f9be9533bb429964592
java@012 <daniyal.digitalwaze@fmail.com>
2021-06-02 13:51:57 +0500

    socket events working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const socket = require('socket.io')
 const express = require('express')
-const path =require('path')
+const path = require('path')
 
 const publicPath = path.join(__dirname, '/public')
 const app = express()
@@ -12,13 +12,23 @@ app.use(express.static(publicPath))
 
 io.on('connection', (socket) => {
     console.log('Connected.....')
-//   socket.on('event', data => { console.log(data) });
-  socket.on('disconnect', () => { 
-      console.log("disconnected")
-   });
+    //   socket.on('event', data => { console.log(data) });
+
+    socket.on('createMessage', (msg) => {
+        console.log('createMessage', msg)
+        io.emit('newMsg', {
+            from: msg.from,
+            text: msg.text,
+            createdAt: new Date().getTime()
+        })
+    })
+
+    socket.on('disconnect', () => {
+        console.log("disconnected")
+    });
 });
 
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log("hello activated", publicPath)
 });
 
