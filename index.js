2ff90f87f91c13653a3b6dee150a90d587e9b30a
java@012 <daniyal.digitalwaze@fmail.com>
2021-06-08 14:14:02 +0500

    updated

--- index.js
+++ index.js
@@ -8,22 +8,36 @@ const app = express()
 let server = http.createServer(app);
 let io = socket(server);
 
+const {Users} = require('./utils/user')
+let users = new Users();
+
 app.use(express.static(publicPath))
 
 io.on('connection', (socket) => {
+    console.log(socket.id, 'socket.id');
     console.log('Connected.....')
-    //   socket.on('event', data => { console.log(data) });
-
-    socket.on('createMessage', (msg) => {
-        console.log('createMessage', msg)
-        io.emit('newMsg', {
-            from: msg.from,
-            text: msg.text,
-            createdAt: new Date().getTime()
-        })
+    
+    socket.on('joinRoom',(params)=>{
+        console.log(params,'params')
+        socket.join(params.room)
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
     })
+    
+    socket.on('createMessage', (msg) => {
+        let user = users.getUser(socket.id)
+            io.to(user.room).emit('newMsg', {
+                from: msg.from,
+                text: msg.text,
+                createdAt: new Date().getTime()
+            })
+        
+      })
 
     socket.on('disconnect', () => {
+        let user = users.getUser(socket.id)
+        users.removeUser(socket.id);
+        socket.leave(user.room)
         console.log("disconnected")
     });
 });
